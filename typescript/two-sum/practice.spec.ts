5e812fbb62f54e5f101bece7bcc4093b64c4df09
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-18 02:28:56 +0000

    tests: splitted the tests into separate files

---
+++ typescript/two-sum/practice.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from "vitest";
+import { twoSum } from "./practice";
+
+describe("Practice test", () => {
+  it("should return [0, 1] when the numbers are [2, 7, 11, 13] and the target is 9", () => {
+    const numbers = [2, 7, 11, 13];
+    const target = 9;
+    expect(twoSum(numbers, target)).toEqual([0, 1]);
+  });
+
+  it("should return [1, 3] when the numbers are [13, 7, 11, 2] and the target is 9", () => {
+    const numbers = [13, 7, 11, 2];
+    const target = 9;
+    expect(twoSum(numbers, target)).toEqual([1, 3]);
+  });
+
+  it("should return [2, 3] when the numbers are [13, 11, 7, 2] and the target is 9", () => {
+    const numbers = [13, 11, 7, 2];
+    const target = 9;
+    expect(twoSum(numbers, target)).toEqual([2, 3]);
+  });
+
+  it("should return [0, 1] when the numbers are [13, 11, 7, 2] and the target is 24", () => {
+    const numbers = [13, 11, 7, 2];
+    const target = 24;
+    expect(twoSum(numbers, target)).toEqual([0, 1]);
+  });
+
+  it("should return [0, 3] when the numbers are [13, 11, 7, 2] and the target is 15", () => {
+    const numbers = [13, 11, 7, 2];
+    const target = 15;
+    expect(twoSum(numbers, target)).toEqual([0, 3]);
+  });
+});
