29d80030e31868c2331ce59d628bec12df8c16c6
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-20 16:20:08 -0500

    feat: Add functionality to get available languages

--- misc/leetcode-cli/index.ts
+++ misc/leetcode-cli/index.ts
@@ -1,6 +1,7 @@
-import Helper from "./utils/helper.ts";
-import * as clack from "@clack/prompts";
+import fs from "node:fs/promises";
 import { execSync } from "node:child_process";
+import * as clack from "@clack/prompts";
+import Helper from "./utils/helper.ts";
 
 const start = async () => {
   Helper.configure();
@@ -10,41 +11,50 @@ const start = async () => {
       " LeetCode Solutions Submit - Luis Osio Chico " +
       "\x1b[0m"
   );
-  clack.note("The usage is: [language] [exercise]");
+  clack.note(
+    "Thanks for using my project, if you like it, please give it a star on GitHub!"
+  );
 
   if (!Helper.session || !Helper.csrfToken) {
     clack.outro("[!] Check the .env file and try again");
     return;
   }
 
-  const command = (await clack.text({
-    message: "Please enter the command you want to run:",
-    placeholder: "typescript two-sum",
+  const languages = await getLanguages();
+
+  const language = (await clack.select({
+    message: "Please select the language you want to use:",
+    options: [
+      ...languages.map((language) => ({
+        value: language.toLowerCase(),
+        label: language,
+      })),
+    ],
+  })) as string;
+
+  const exercise = (await clack.text({
+    message: "Please enter the exercise you want to run:",
+    placeholder: "two-sum",
     validate: (input) => {
-      if (input.length === 0 || input.split(" ").length !== 2)
-        return "Please enter a valid command: [language] [exercise]";
-
-      const [language, exercise] = input.split(" ");
-
-      try {
-        const excludedFolders = [".idx", ".vscode", "misc"];
-        if (excludedFolders.some((folder) => language.includes(folder)))
-          throw Error("Excluded folder");
-
-        execSync(`cd ../.././${language}`);
-      } catch {
-        return "Please enter a valid language: [typescript, scala, etc]";
-      }
-
-      try {
-        execSync(`cd ../.././${language}/${exercise}`);
-      } catch {
-        return "Exercise not found, please check the command";
-      }
+      if (input.length === 0) return "Please enter a valid command: [exercise]";
     },
   })) as string;
 
-  const [language, exercise] = command!.split(" ");
+  try {
+    const excludedFolders = [".idx", ".vscode", "misc"];
+    if (excludedFolders.some((folder) => language.includes(folder)))
+      throw Error("Excluded folder");
+
+    execSync(`cd ../.././${language}`);
+  } catch {
+    return "Please enter a valid language: [typescript, scala, etc]";
+  }
+
+  try {
+    execSync(`cd ../.././${language}/${exercise}`);
+  } catch {
+    return "Exercise not found, please open a PR to add it";
+  }
 
   const confirm = await clack.confirm({
     message: `Are you sure you want to submit the exercise '${exercise}' in the language '${language}'?`,
@@ -54,6 +64,22 @@ const start = async () => {
     clack.outro("[!] Exiting...");
     return;
   }
+};
+
+const getLanguages = async () => {
+  const readme = await fs.readFile("../../README.md", "utf-8");
+  if (!readme) throw new Error("README.md not found");
+
+  const table = readme
+    .split("## Languages")[1]
+    .split("## Usage")[0]
+    .split("\r\n");
+  const lines = table.filter((line) => line.trim().length > 0).splice(2);
+  const languages = lines.map(
+    (line) => line.split("|").map((l) => l.trim())[1]
+  );
+
+  return languages;
 };
 
 void start();
