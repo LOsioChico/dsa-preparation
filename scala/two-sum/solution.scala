d23b677ddcb981c48860849c2310f6e87779ed42
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-20 03:45:25 +0000

    docs: Update problem description and examples

--- scala/two-sum/solution.scala
+++ scala/two-sum/solution.scala
@@ -2,20 +2,28 @@
 
 import scala.collection.mutable.HashMap
 
-/** @tag
-  *   Array; Hash Table
-  *
+/** scalafmt: { comments.style = "off"}
+  * @tag Array; Hash Table
+  ***************************************************************************
   * Given an array of integers, find two numbers such that they add up to a
   * specific target number. The function twoSum should return indices of the two
-  * numbers such that they add up to the target, where index1 must be less than
-  * index2. Please note that your returned answers (both index1 and index2) are
-  * not zero-based.
+  * numbers such that they add up to the target, where the first must be less
+  * than the second.
   *
-  * You may assume that each input would have exactly one solution. Input
-  * numbers = { 2, 7, 11, 15 }, target = 9 Output [0, 1]
+  * You may assume that each input would have exactly one solution.
+  * 
+  * Input: numbers = (2,7,11,15), target = 9
+  * Output: (0,1)
+  * Explanation: Because numbers(0) + numbers(1) == 9, we return (0,1)
+  * 
+  * Input: nums = (3,2,4), target = 6
+  * Output: (1,2)
+  * 
+  * Input: nums = (3,3), target = 6
+  * Output: (0,1)
   *
-  * @link
-  *   https://leetcode.com/problems/two-sum/
+  ***************************************************************************
+  * { @link https://leetcode.com/problems/two-sum/ }
   */
 
 object Solution {
--- typescript/two-sum/practice.ts
+++ typescript/two-sum/practice.ts
@@ -1,20 +1,26 @@
 /**
- * @tag Array; Hash Table
- ***************************************************************************
- * Given an array of integers, find two numbers such that they add up to
- * a specific target number.
- * The function twoSum should return indices of the two numbers such that
- * they add up to the target, where index1 must be less than index2.
- * Please note that your returned answers (both index1 and index2) are not
- * zero-based.
- *
- * You may assume that each input would have exactly one solution.
- * Input numbers = {2, 7, 11, 15}, target = 9
- * Output index1 = 1, index2 = 2
- *
- ***************************************************************************
- * {@link https://leetcode.com/problems/two-sum/ }
- */
+  * @tag Array; Hash Table
+  ***************************************************************************
+  * Given an array of integers, find two numbers such that they add up to a
+  * specific target number. The function twoSum should return indices of the two
+  * numbers such that they add up to the target, where the first must be less
+  * than the second.
+  *
+  * You may assume that each input would have exactly one solution.
+  * 
+  * Input: numbers = [2,7,11,15], target = 9
+  * Output: [0,1]
+  * Explanation: Because numbers[0] + numbers[1] == 9, we return [0,1]
+  * 
+  * Input: nums = [3,2,4], target = 6
+  * Output: [1,2]
+  * 
+  * Input: nums = [3,3], target = 6
+  * Output: [0,1]
+  *
+  ***************************************************************************
+  * { @link https://leetcode.com/problems/two-sum/ }
+  */
 
 export const twoSum = (numbers: number[], target: number) => {
   //
