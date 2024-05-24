51c9dc1cbd7cc9d6abfc8c890fa2a8ac996b35c8
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-24 14:58:45 -0500

    feat: Add kebabToCamelCase function

--- misc/leetcode-cli/utils/utils.ts
+++ misc/leetcode-cli/utils/utils.ts
@@ -20,3 +20,7 @@ export const getLanguagesFromReadme = async () => {
 
   return languages;
 };
+
+export const kebabToCamelCase = (str: string) => {
+  return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+};
