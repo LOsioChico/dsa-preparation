2ae40bf71bc55750d9e4986e51df04201d571d5d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-24 14:29:27 +0000

    docs: Add documentation for containsDuplicate solution

--- algorithms/scala/contains-duplicate/solution.scala
+++ algorithms/scala/contains-duplicate/solution.scala
@@ -2,6 +2,25 @@
 
 package containsDuplicate
 
+/**
+ * @tag Array; Hash Table; Sorting
+ ***************************************************************************
+ * Given an integer array numbers, return true if any value appears at least
+ * twice in the array, and return false if every element is distinct.
+ *
+ * Input: numbers = (1,2,3,1)
+ * Output: true
+ *
+ * Input: numbers = (1,2,3,4)
+ * Output: false
+ *
+ * Input: numbers = (1,1,1,3,3,4,3,2,4,2)
+ * Output: true
+ *
+ ***************************************************************************
+ *  { @link https://leetcode.com/problems/contains-duplicate/ }
+ */
+
 object Solution {
   def containsDuplicateNestedLoops(numbers: Array[Int]): Boolean = ???
   def containsDuplicateSorting(numbers: Array[Int]): Boolean = ???
