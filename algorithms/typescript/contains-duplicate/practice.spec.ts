d053a034ec370f1e798abab470900e032c08cad4
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-24 05:07:45 +0000

    test: Update test cases format to include spaces between numbers

--- algorithms/typescript/contains-duplicate/practice.spec.ts
+++ algorithms/typescript/contains-duplicate/practice.spec.ts
@@ -2,17 +2,17 @@ import { describe, it, expect } from "vitest";
 import containsDuplicate from "./practice";
 
 describe("Contain Duplicates Solution tests", () => {
-  it("Should return true when the numbers are [1,2,3,1]", () => {
+  it("Should return true when the numbers are [1, 2, 3, 1]", () => {
     const numbers = [1, 2, 3, 1];
     expect(containsDuplicate(numbers)).toBeTruthy();
   });
 
-  it("Should return false when the numbers are [1,2,3,4]", () => {
+  it("Should return false when the numbers are [1, 2, 3, 4]", () => {
     const numbers = [1, 2, 3, 4];
     expect(containsDuplicate(numbers)).toBeFalsy();
   });
 
-  it("Should return true when the numbers are [1,1,1,3,3,4,3,2,4,2]", () => {
+  it("Should return true when the numbers are [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]", () => {
     const numbers = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
     expect(containsDuplicate(numbers)).toBeTruthy();
   });
