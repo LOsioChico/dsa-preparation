24937b3d486981f6e5fc0d1140171b4b1ec03547
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-24 14:23:46 +0000

    fix: Update variable name to match function parameter name

--- algorithms/typescript/contains-duplicate/solution.ts
+++ algorithms/typescript/contains-duplicate/solution.ts
@@ -4,13 +4,13 @@
  * Given an integer array numbers, return true if any value appears at least
  * twice in the array, and return false if every element is distinct.
  *
- * Input: nums = [1,2,3,1]
+ * Input: numbers = [1,2,3,1]
  * Output: true
  *
- * Input: nums = [1,2,3,4]
+ * Input: numbers = [1,2,3,4]
  * Output: false
  *
- * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Input: numbers = [1,1,1,3,3,4,3,2,4,2]
  * Output: true
  *
  ***************************************************************************
