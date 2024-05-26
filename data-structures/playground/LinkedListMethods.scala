420a8dcfbf1d6e75ecc96385d4c9ea5f4aca48c9
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-26 02:17:40 +0000

    refactor: Update LinkedList class to use Empty instead of NilO

--- data-structures/playground/LinkedListMethods.scala
+++ data-structures/playground/LinkedListMethods.scala
@@ -1,5 +1,9 @@
 //> using scala "3.3.1"
 
+@main def main =
+  val linkedList = LinkedList(1, LinkedList(2, LinkedList(3, Empty)))
+  println(linkedList.map(x => x * 2))
+
 sealed trait LinkedListI:
   def map(fn: Int => Int): LinkedListI
 
@@ -7,9 +11,5 @@ case class LinkedList(head: Int, tail: LinkedListI) extends LinkedListI:
   def map(fn: Int => Int): LinkedListI =
     LinkedList(fn(head), tail.map(fn))
 
-case object NilO extends LinkedListI:
-  def map(fn: Int => Int): LinkedListI = NilO
-
-@main def main =
-  val listN = LinkedList(1, LinkedList(2, LinkedList(3, NilO)))
-  println(listN.map(x => x * 2))
+case object Empty extends LinkedListI:
+  def map(fn: Int => Int): LinkedListI = Empty
