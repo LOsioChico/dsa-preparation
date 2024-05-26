69af1e3552157fb9cf7915420fe29143abb9046e
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-26 03:24:59 +0000

    refactor: Update linked list implementation with new methods

--- data-structures/playground/LinkedListMethods.scala
+++ data-structures/playground/LinkedListMethods.scala
@@ -1,15 +1,31 @@
 //> using scala "3.3.1"
+import java.util.NoSuchElementException
 
-@main def main =
-  val linkedList = LinkedList(1, LinkedList(2, LinkedList(3, Empty)))
-  println(linkedList.map(x => x * 2))
+@main def main = {
+  val linkedList = LinkedList(1, 2, 3, 4, 5)
+  println(linkedList)
+}
 
-sealed trait LinkedListI:
-  def map(fn: Int => Int): LinkedListI
+sealed trait LinkedListT {
+  def head: Int
+  def tail: LinkedListT
+  def prepend: (num: Int) => LinkedListT
+  def append: (num: Int) => LinkedListT
+  def map: (fn: Int => Int) => LinkedListT
+}
 
-case class LinkedList(head: Int, tail: LinkedListI) extends LinkedListI:
-  override def map(fn: Int => Int): LinkedListI =
-    LinkedList(fn(head), tail.map(fn))
+case object LinkedList extends LinkedListT {
+  def apply(nums: Int*) =
+    nums.foldRight(LinkedList: LinkedListT)((num, acc) => Constructor(num, acc))
+  def head = throw new NoSuchElementException("Head of empty linked list")
+  def tail = throw new NoSuchElementException("Tail of empty linked list")
+  def prepend = (num: Int) => Constructor(num, this)
+  def append = (num: Int) => Constructor(num, this)
+  def map = (_) => LinkedList
+}
 
-case object Empty extends LinkedListI:
-  override def map(fn: Int => Int): LinkedListI = Empty
+case class Constructor(head: Int, tail: LinkedListT) extends LinkedListT {
+  def prepend = (num: Int) => Constructor(num, this)
+  def append = (num: Int) => Constructor(head, tail.append(num))
+  def map = (fn: Int => Int) => Constructor(fn(head), tail.map(fn))
+}
