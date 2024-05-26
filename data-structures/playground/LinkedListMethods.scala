efa454cee4e7646ab81e1c99c110d44975cf2628
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-26 02:18:50 +0000

    refactor: Override map method in LinkedList and Empty

--- data-structures/playground/LinkedListMethods.scala
+++ data-structures/playground/LinkedListMethods.scala
@@ -8,8 +8,8 @@ sealed trait LinkedListI:
   def map(fn: Int => Int): LinkedListI
 
 case class LinkedList(head: Int, tail: LinkedListI) extends LinkedListI:
-  def map(fn: Int => Int): LinkedListI =
+  override def map(fn: Int => Int): LinkedListI =
     LinkedList(fn(head), tail.map(fn))
 
 case object Empty extends LinkedListI:
-  def map(fn: Int => Int): LinkedListI = Empty
+  override def map(fn: Int => Int): LinkedListI = Empty
