ed5cc1e1005e7f46033660569dc0766c8dd1835d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-27 03:04:35 +0000

    feat: Add isAnagramHashTableArray function and test case

--- algorithms/typescript/valid-anagram/solution.spec.ts
+++ algorithms/typescript/valid-anagram/solution.spec.ts
@@ -1,14 +1,20 @@
 import { describe, it, expect } from "vitest";
-import { isAnagramHashTable, isAnagramSorting } from "./solution";
+import {
+  isAnagramHashTable,
+  isAnagramSorting,
+  isAnagramHashTableArray,
+} from "./solution";
 
 describe("Valid Anagram Solution tests", () => {
   it('Should return true when params are s = "anagram", t = "nagaram"', () => {
     expect(isAnagramHashTable("anagram", "nagaram")).toBeTruthy();
     expect(isAnagramSorting("anagram", "nagaram")).toBeTruthy();
+    expect(isAnagramHashTableArray("anagram", "nagaram")).toBeTruthy();
   });
 
   it('Should return false when params are s = "rat", t = "cat"', () => {
     expect(isAnagramHashTable("rat", "car")).toBeFalsy();
     expect(isAnagramSorting("rat", "car")).toBeFalsy();
+    expect(isAnagramHashTableArray("rat", "car")).toBeFalsy();
   });
 });
--- algorithms/typescript/valid-anagram/solution.ts
+++ algorithms/typescript/valid-anagram/solution.ts
@@ -16,6 +16,9 @@
  * { @link https://leetcode.com/problems/valid-anagram/ }
  */
 
+// Hash Table Two Pass - O(2n) → O(n) time and O(26) → O(1) space
+// After valid the length iterate with index over both of strings, then
+// increase w/s and decrease w/t the counter based on the char
 export function isAnagramHashTable(s: string, t: string): boolean {
   if (s.length != t.length) return false;
 
@@ -31,4 +34,24 @@ export function isAnagramHashTable(s: string, t: string): boolean {
 
 export function isAnagramSorting(s: string, t: string): boolean {
   return s.split("").sort().join("") == t.split("").sort().join("");
+}
+
+
+// Hash Table Array - O(2n) → O(n) time and O(26) + O(26) → O(1) space
+// After valid the length, make the hashes basing on the contraint
+// the says, s and t consist of lowercase English letters so is 26,
+// then, this is not zero based so we need to decrease the 'a' char
+// that is the first value, then increase count based on index
+export function isAnagramHashTableArray(s: string, t: string): boolean {
+  if (s.length != t.length) return false;
+
+  const hash = Array(26).fill(0);
+  const toIndex = (char: string) => char.charCodeAt(0) - "a".charCodeAt(0);
+
+  for (let i = 0; i < s.length; i++) {
+    hash[toIndex(s[i])]++;
+    hash[toIndex(t[i])]--;
+  }
+
+  return hash.every((count) => count === 0);
 }
